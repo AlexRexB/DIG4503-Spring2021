373e9c082f98642913903163ab2bc45e49e92fac
AlexRexB <alexrexb@gmail.com>
2021-03-07 00:55:20 -0500

    Portfolio

--- lab3/src/App.js
+++ lab3/src/App.js
@@ -1,14 +1,11 @@
 import React from 'react';
+import MyHeader from './components/MyHeader';
 import HomePage from './components/HomePage';
 
 class App extends React.Component {
   render() {
     return (
-      <div>
-      <h1 style={mystyle}>Hello Style!</h1>
-      <p>Add a little style!</p>
-      </div>
-      <HomePage firstName='Alex'/>
+      <MyHeader input type="text"/>
     );
   }
 }
