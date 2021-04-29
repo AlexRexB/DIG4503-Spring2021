683470cb0e0ad6955a890d38dc1036f21d71c26c
AlexRexB <alexrexb@gmail.com>
2021-04-28 23:56:36 -0400

    Lab 12

--- lab11/index.js
+++ lab11/index.js
@@ -17,16 +17,47 @@ const d = new Database();
 d.connect();
 
 // PUT -> d.createOne() -> collection.insertOne()
-App.put("/books/:ISBN", (req, res) => {});
+App.put("/books/:ISBN", (req, res) => {
+    const title = req.body.title;
+    const author = req.body.author;
+    const rating = req.body.rating;
+
+    res.json({
+        title: title,
+        author: author,
+        rating: rating,
+
+    })
+});
 
 // GET -> d.readOne() -> collection.findOne()
-App.get("/books/:ISBN", (req, res) => {});
+App.get("/books/:ISBN", (req, res) => {
+    const title = req.body.title;
+    const author = req.body.author;
+    const rating = req.body.rating;
+
+});
 
 // POST -> One or more database methods
-App.post("/books/search", (req, res) => {});
+App.post("/books/search", (req, res) => {
+    const title = req.body.title;
+    const author = req.body.author;
+
+
+});
 
 // PATCH -> d.updateOne() -> collection.updateOne()
-App.patch("/books/:ISBN", (req, res) => {});
+App.patch("/books/:ISBN", (req, res) => {
+    const title = req.body.title;
+    const author = req.body.author;
+    const rating = req.body.rating;
+    
+    const result = d.updateOne(ISBN);
+
+    res.json(result)
+
+
+});
 
 // DELETE -> d.deleteOne() -> collection.deleteOne()
 App.delete("/books/:ISBN", (req, res) => {
